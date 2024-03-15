ebece5f8be6bf0f535faaa6ea86a893c869fbf63
Davidson <deivisonj1@hotmail.com>
2024-03-15 10:26:31 -0300

    banco de dados

--- loja/aperitivos/urls.py
+++ loja/aperitivos/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from loja.aperitivos.views import   Produto
 
+from loja.aperitivos.views import produto_alguma_coisa, meu_carrinho, adicionar_no_carrinho
+ 
 app_name = 'aperitivos'
+
 urlpatterns = [
-    path('<slug:slug>', Produto, name='Produto'),
-   
-]
+    path('meucarrinho', meu_carrinho),
+    path('meucarrinho/adicionar/<int:produto_id>', adicionar_no_carrinho, name='carrinho.adicionar'),
+    path('<slug:slug>', produto_alguma_coisa, name='Produto'),
+
+ ]
