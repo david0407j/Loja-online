08c8bbc1394320f98c5d7195bd185daac66fb6fc
Davidson <deivisonj1@hotmail.com>
2024-04-18 22:06:40 -0300

    html

--- loja/base/urls.py
+++ loja/base/urls.py
@@ -2,12 +2,12 @@
 from django.urls import path
 
 from loja.base.views import home
-from django.contrib.auth import views as auth_views
+
 
 app_name = 'base'
 
 
 urlpatterns = [
     path('', home, name='home'),
-   path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+   
 ]
--- loja/urls.py
+++ loja/urls.py
@@ -17,6 +17,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -24,6 +25,8 @@ urlpatterns = [
     path('', include('loja.base.urls')),
     path('produtos/', include('loja.produtos.urls')),
     path('contas/', include('django.contrib.auth.urls')),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+   path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
