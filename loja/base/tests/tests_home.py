c6dac13b0925535413e3a126eafba9593398e7e7
Davidson <deivisonj1@hotmail.com>
2024-05-06 22:51:34 -0300

    testando os códigos navbr do projeto

--- loja/base/tests/tests_home.py
+++ loja/base/tests/tests_home.py
@@ -2,8 +2,11 @@ import pytest
 from django.urls import reverse
 from loja.django_assertions import assert_contains
 
+# tests navb link dos produtos 
+
+ 
 @pytest.fixture
-def resp(client):
+def resp(client,):
    resp = client.get(reverse('base:home'))
    return resp
 def test_status_code(resp):
@@ -15,13 +18,20 @@ def test_title(resp):
 def test_home_link(resp):
    assert_contains(resp, f'href="{reverse("base:home")}">Reizinho-importe</a>')
 
-def test_conteudo_link(resp):
-    assert_contains(resp, 'href="http://127.0.0.1:8000/aperitivos/masculino"')
+def test_pagina_deve_conter_o_link_produtos_masculinos(resp):
+    assert_contains(resp, reverse('produtos:produto', kwargs={'nome_categoria':'Masculino'}))
+    
 def test_email(resp):
    assert_contains(resp, 'reizinhomp@gmail.com')
 
-def test_conteudo_link_infatil(resp):
-    assert_contains(resp, 'href="http://127.0.0.1:8000/modelos/femenina"')
+def test_pagina_o_link_infantil(resp):
+    assert_contains(resp, reverse('produtos:produto', kwargs={'nome_categoria':'Infantil'}))
 
-def test_conteudo_link(resp):
-    assert_contains(resp, 'href="http://127.0.0.1:8000/produtos/outros"')
+def test_pagina_deve_conter_o_link_produtos(resp):
+    assert_contains(resp, reverse('produtos:produto', kwargs={'nome_categoria':'Produtos'}))
+
+def test_pagina_deve_conter_o_link_feminina(resp):
+    assert_contains(resp, reverse('produtos:produto', kwargs={'nome_categoria':'Feminina'}))
+
+def test_pagina_deve_conter_o_link_cosmetico(resp):
+    assert_contains(resp, reverse('produtos:produto', kwargs={'nome_categoria':'Cosmético'}))
